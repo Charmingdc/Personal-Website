d3928a2e20e82d21d5c0781f0669b77e23013de4
Adebayo Muis <charmingdc002@gmail.com>
2025-01-26 21:26:30 +0100

    Minor change

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,3 +1,5 @@
+import { motion, MotionConfig } from 'motion/react';
+
 import SvgIcons from './SvgIcons.tsx';
 import ProjectBox from './ProjectBox.tsx';
 import useProjects from '../hooks/useProjects.tsx';
@@ -16,16 +18,34 @@ interface Project {
 const ProjectSection = () => {
   const projects: Project[] = useProjects();
   
+  const variants = {
+    hidden: { opacity: 0 },
+    active: { opacity: 1 }
+  }
+  
   return (
     <section>
-      <div className="section-headtag">
-        <h3>Portfolio</h3>
-        <SvgIcons type="rightDownArrow" />
-      </div>
+       <MotionConfig 
+        transition={{ duration: 1, ease: 'easeOut'}}>
+         <motion.div 
+          className='section-headtag'
+          variants={variants}
+          initial="hidden"
+          whileInView="active">
+           <h3> Portfolio </h3>
+        
+           <SvgIcons type='rightDownArrow' width='40px' height='40px' />
+        </motion.div>
       
-      <h2 className="section-headtitle">
-        Design, tech & some magic
-      </h2>
+        <motion.h1
+          className='section-headtitle'
+          variants={variants}
+          initial="hidden"
+          whileInView="active"> 
+           Design, tech & some magics
+        </motion.h1>
+      </MotionConfig>
+     
       
       <p>
         Blending mixes of the latest technologies to create innovative and problem-solving projects. Here are some of my works.
